47c65efd9f4c7e6a935c7ba081a2522f42a481cc
MichalLukac <cospelthetraceur@gmail.com>
2020-09-18 15:39:10 +0200

    Update to pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
 import os
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="tf_metric_learning",
+    name="tf-metric-learning",
     version="0.1.0",
     description="Image similarity, metric learning loss functions for TensorFlow 2+.",
     url="https://github.com/Ximilar-com/tf-metric-learning",
-    author="Ximilar.com Team",
+    author="Michal Lukac & Ximilar.com Team",
     author_email="tech@ximilar.com",
     license="MIT",
     packages=find_packages(),
@@ -20,4 +24,6 @@ setup(
     include_package_data=True,
     zip_safe=False,
     namespace_packages=["tf_metric_learning"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
